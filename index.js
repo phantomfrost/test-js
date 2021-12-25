ff6c1d520025ae09944d0bf9b44d5b6097fe21bd
phantomfrost <78469955+phantomfrost@users.noreply.github.com>
2021-12-25 16:42:45 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+!function()
+{
+	const Test = "测试"
+	let xue_html=`
+<!doctype html>
+<html lang="zh">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+		<title>${Test}</title>
+	</head>
+	<body>
+		<div>
+			<span>Div_Name:</span>
+			<span style="font-size: 100px;">${Div_Name}</span>
+	</body>
+</html>
+	`
+	return new Response(hpp_installhtml,{headers: { "content-type": "text/html;charset=UTF-8" }});
+}
