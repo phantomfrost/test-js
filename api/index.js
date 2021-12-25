261eeb9d137d2ad692928a8524a41bdad6a05e94
phantomfrost <78469955+phantomfrost@users.noreply.github.com>
2021-12-25 17:54:12 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,20 +1,25 @@
-export default function handler(response) {
-	const Test = "测试";
-	// let xue_html=`
-	// 	<!doctype html>
-	// 	<html lang="zh">
-	// 		<head>
-	// 			<meta charset="UTF-8">
-	// 			<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
-	// 			<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
-	// 			<title>${Test}</title>
-	// 		</head>
-	// 		<body>
-	// 			<div>
-	// 				<span>Div_Name:</span>
-	// 				<span style="font-size: 100px;">${Div_Name}</span>
-	// 		</body>
-	// 	</html>`;
-	// res.status(200).send(xue_html);
-	res.status(200).send(`Hello , ${Test}`);
+// export default function handler(response) {
+// 	const Test = "测试";
+// 	// let xue_html=`
+// 	// 	<!doctype html>
+// 	// 	<html lang="zh">
+// 	// 		<head>
+// 	// 			<meta charset="UTF-8">
+// 	// 			<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+// 	// 			<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+// 	// 			<title>${Test}</title>
+// 	// 		</head>
+// 	// 		<body>
+// 	// 			<div>
+// 	// 				<span>Div_Name:</span>
+// 	// 				<span style="font-size: 100px;">${Div_Name}</span>
+// 	// 		</body>
+// 	// 	</html>`;
+// 	// res.status(200).send(xue_html);
+// 	res.status(200).send(`Hello , ${Test}`);
+// }
+
+export default function handler(request, response) {
+  const { name } = req.query;
+  res.status(200).send(`Hello ${name}!`);
 }
