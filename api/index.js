86fbdec6a340d8fb509817b5ab1a529715695652
phantomfrost <78469955+phantomfrost@users.noreply.github.com>
2021-12-25 17:59:34 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 export default function handler(request, response) {
-  const { name } = req.query;
-  res.status(200).send(`Hello ${name}!`);
+  const { name } = request.query;
+  response.status(200).send(`Hello ${name}!`);
 }
