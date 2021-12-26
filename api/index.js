95231916bd1bf1f01c0be3bd72fd1760a0b614a8
phantomfrost <78469955+phantomfrost@users.noreply.github.com>
2021-12-26 11:26:05 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 export default function handler(request,response) {
   const name = "test";
+  const path = require('path').resolve('./');
   response.status(200).send(`Hello ${name}, ${process.env.Div_Name} , ${path}!`);
 }
